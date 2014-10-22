32b436ea5c75ba4ade43b93f838250e2f562e110
Teera653 <sab_za_nk60@hotmail.com>
2014-10-22 12:09:25 +0700

    top

---
+++ gen/buusk/example/cient_server_55410653/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package buusk.example.cient_server_55410653;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
